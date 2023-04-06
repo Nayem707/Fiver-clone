964d807bdf7f81ea726b8d8af4702b29924d5434
nayem islam <gangsn490@gmail.com>
2023-04-07 03:00:00 +0600

    update

--- cli/src/components/footer/Footer.jsx
+++ cli/src/components/footer/Footer.jsx
@@ -1,27 +1,108 @@
 import React from 'react';
 import './footer.scss';
+import { FaFacebook } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className='footr'>
+    <div className='footer'>
       <div className='container'>
         <div className='top'>
-          <h2>catagory</h2>
-          <span>graphics desgin</span>
-          <span>graphics desgin</span>
-          <span>graphics desgin</span>
-          <span>graphics desgin</span>
-          <span>graphics desgin</span>
-          <span>graphics desgin</span>
-          <span>graphics desgin</span>
-          <span>graphics desgin</span>
-          <span>graphics desgin</span>
-          <span>graphics desgin</span>
-          <span>graphics desgin</span>
+          <div className='item'>
+            <h2>catagory 1</h2>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+          </div>
+
+          <div className='item'>
+            <h2>catagory 2</h2>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+          </div>
+          <div className='item'>
+            <h2>catagory 3</h2>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+          </div>
+          <div className='item'>
+            <h2>catagory 4</h2>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+          </div>
+          <div className='item'>
+            <h2>catagory 5</h2>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+            <span>graphics desgin</span>
+          </div>
         </div>
+        <hr />
         <div className='bottom'>
-          <div className='left'></div>
-          <div className='right'></div>
+          <div className='left'>
+            <h2>fiver</h2>
+            <span>@ Fiver international ltd. 2023</span>
+          </div>
+          <div className='right'>
+            <div className='social'>
+              <FaFacebook />
+              <FaFacebook />
+              <FaFacebook />
+              <FaFacebook />
+              <FaFacebook />
+            </div>
+            <div className='link'>
+              <FaFacebook />
+              English
+            </div>
+            <div className='link'>
+              <FaFacebook />
+              USD
+            </div>
+            <FaFacebook />
+          </div>
         </div>
       </div>
     </div>
