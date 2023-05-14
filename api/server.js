effa994e910b84a953a107adb8f35f90602cc7b4
nayem islam <gangsn490@gmail.com>
2023-05-14 10:29:36 +0600

    dotenv

--- api/server.js
+++ api/server.js
@@ -1,8 +1,16 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+
+try {
+  await mongoose.connect(process.env.MONGO_URI);
+} catch (error) {
+  console.log('connectDB');
+}
 
 app.listen(8800, () => {
-  console.log('server is runing! 8800');
+  console.log('server run 8800');
 });
