24c3a927daee952ce6bbe27bcd1492dd1a975b33
nayem islam <gangsn490@gmail.com>
2023-05-23 11:39:49 +0600

    auth created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,9 +1,30 @@
-export const register = (req, res) => {
-  res.send('works');
+export const register = async (req, res) => {
+  try {
+    const newUser = new User({
+      username: 'test',
+      email: 'test@gmail.com',
+      password: '12345',
+      country: 'testorydesh',
+    });
+
+    await newUser.save();
+
+    res.status(201).send('user created!');
+  } catch (error) {
+    res.status(500).send('user not creted!');
+  }
 };
-export const login = (req, res) => {
-  res.send('works');
+
+export const login = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).send('something went wrong!');
+  }
 };
-export const logout = (req, res) => {
-  res.send('works');
+
+export const logout = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).send('something went wrong!');
+  }
 };
--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 //ROUTES
+import authRoute from './routes/auth.route.js';
 import userRoute from './routes/user.route.js';
 // import gigRoute from './routes/gig.route.js';
 // import orderRoute from './routes/order.route.js';
@@ -22,6 +23,8 @@ const connect = async () => {
   }
 };
 
+//API ROUTES USE
+app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 // app.use('/api/gigs', gigRoute);
 // app.use('/api/orders', orderRoute);
